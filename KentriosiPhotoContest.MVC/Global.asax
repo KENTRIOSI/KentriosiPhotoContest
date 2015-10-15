22d031b9c46d2884ef90f6658a92b6b4e514f2f5
ttitto <ttitto_prog@vfemail.net>
2015-10-15 20:48:32 +0300

    Initial setup. MVC and Models projects created

---
+++ KentriosiPhotoContest.MVC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="KentriosiPhotoContest.MVC.MvcApplication" Language="C#" %>
---
+++ KentriosiPhotoContest.MVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(KentriosiPhotoContest.MVC.Startup))]
+namespace KentriosiPhotoContest.MVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
