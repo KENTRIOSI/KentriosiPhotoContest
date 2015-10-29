ede59ba6528071d0a7ca91a0abd18f442c1e21f3
ttitto <ttitto_prog@vfemail.net>
2015-10-29 12:41:57 +0200

    fix in ContestViewModel - mapping was not implemented

--- KentriosiPhotosContests.MVC/Models/ViewModels/ContestViewModel.cs
+++ KentriosiPhotosContests.MVC/Models/ViewModels/ContestViewModel.cs
@@ -9,17 +9,17 @@
     using System.Threading.Tasks;
     using AutoMapper;
 
-    public class ContestViewModel :IMapFrom<Contest> 
-        //IHaveCustomMappings
+    public class ContestViewModel : IMapFrom<Contest>, IHaveCustomMappings
     {
-        public int ContestImageId { get; set; }
+        public int? ContestImageId { get; set; }
 
-        public string  ContestImageUrl { get; set; }
+        public string ContestImageUrl { get; set; }
 
-        //TODO
-        //public void CreateMappings(IConfiguration configuration)
-        //{
-        //    throw new NotImplementedException();
-        //}
+        public void CreateMappings(IConfiguration configuration)
+        {
+            configuration.CreateMap<Contest, ContestViewModel>()
+                .ForMember(cvm => cvm.ContestImageUrl, opt => opt.MapFrom(c => c.ContestImage.Path))
+                .ReverseMap();
+        }
     }
 }
